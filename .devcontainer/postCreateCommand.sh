8191ffd4df568c776f5cb2b6607d415b9fe6bbf9
Alex <tetrisiq@tuta.io>
2023-01-22 21:02:35 +0000

    feat: moving filewatcher to sh file and add npm script

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -10,7 +10,3 @@ mkdir logs
 mkdir data
 sudo chown node logs
 sudo chown node data 
-# We need to change the owner of the files in the app-data folder
-# if this failes we have to change the permission your self
-fswatch --event=Created /workspaces/runtipi/app-data/ | \
-   xargs -l1 sh -c 'echo "$1" && sudo chown node "$1" -R' -- &

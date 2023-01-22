fea4aa7aace04fadcb3a91ffd567d654c2c6c0b1
Alex <tetrisiq@tuta.io>
2023-01-22 20:38:53 +0000

    feat: adding fswatch for file changes in app-data

--- .devcontainer/createStateSettings.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-echo '{ 
-    "appsRepoUrl": "https://github.com/meienberger/runtipi-appstore.git/",
-}' 
--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-./.devcontainer/createStateSettings.sh > state/settings.json
+echo '{ 
+    "appsRepoUrl": "https://github.com/meienberger/runtipi-appstore.git/"
+}' > state/settings.json
 npm i -g pnpm
 pnpm i
 sudo apt-get update
@@ -7,4 +9,8 @@ sudo apt-get install jq fswatch -y
 mkdir logs
 mkdir data
 sudo chown node logs
-sudo chown node data
+sudo chown node data 
+# We need to change the owner of the files in the app-data folder
+# if this failes we have to change the permission your self
+fswatch --event=Created /workspaces/runtipi/app-data/ | \
+   xargs -l1 sh -c 'echo "$1" && sudo chown node "$1" -R' -- &
